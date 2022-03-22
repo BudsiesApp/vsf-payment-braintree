8265f9f2ef9a7844e1be766d029b66dbc71b37e1
Anton Sylka <anton.sylka@gorbunov.studio>
2022-03-22 15:09:27 +0800

    Refs #26146: CheckoutCard - improve fields validation and errors handling

--- components/CheckoutCard.vue
+++ components/CheckoutCard.vue
@@ -4,36 +4,58 @@
 
     <div class="_content" v-show="isSelected">
       <div class="_row">
-        <div class="_field">
-          <label for="card-number" class="_label">Card Number</label>
+        <div class="_field" :class="{ '-error': showNumberError }">
+          <label for="card-number" class="_label">
+            {{ $t('Card Number') }}
+          </label>
           <div id="card-number" class="_input" />
+          <div class="_bar" />
         </div>
       </div>
 
       <div class="_row">
-        <div class="_field">
-          <label for="cvv" class="_label">CVV</label>
+        <div class="_field" :class="{ '-error': showCvvError }">
+          <label for="cvv" class="_label">
+            {{ $t('CVV') }}
+          </label>
           <div id="cvv" class="_input" />
+          <div class="_bar" />
         </div>
 
-        <div class="_field">
-          <label for="expiration-date" class="_label">Expiration Date</label>
+        <div class="_field" :class="{ '-error': showExpirationDateError }">
+          <label for="expiration-date" class="_label">
+            {{ $t('Expiration Date') }}
+          </label>
           <div id="expiration-date" class="_input" />
+          <div class="_bar" />
         </div>
+      </div>
+
+      <div class="_error-message">
+        {{ errorMessage }}
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import braintree, { HostedFields } from 'braintree-web';
+import braintree, { BraintreeError, HostedFields, HostedFieldsEvent } from 'braintree-web';
 import Vue, { PropType, VueConstructor } from 'vue'
+import { TranslateResult } from 'vue-i18n';
+
 import { InjectType } from 'src/modules/shared';
 
 import TransactionData from '../types/transaction-data';
+import { Dictionary } from 'src/modules/budsies';
 
 interface InjectedServices {
   window: Window
+}
+
+const enum Fields {
+  NUMBER = 'number',
+  EXPIRATION_DATE = 'expirationDate',
+  CVV = 'cvv'
 }
 
 export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
@@ -56,9 +78,30 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
     }
   },
   data () {
+    const fieldsValidationState: Dictionary<boolean> = {
+      'number': true,
+      'cvv': true,
+      'expirationDate': true
+    };
+
     return {
       hostedFieldsInstance: undefined as undefined | HostedFields,
-      fSubmitHandler: undefined as undefined | ((event: MouseEvent) => Promise<void>)
+      fSubmitHandler: undefined as undefined | ((event: MouseEvent) => Promise<void>),
+      fieldsValidationState,
+      errorMessage: '' as TranslateResult,
+      fOnHostedFieldsBlur: undefined as ((event: HostedFieldsEvent) => void) | undefined,
+      fOnHostedFieldsFocus: undefined as ((event: HostedFieldsEvent) => void) | undefined
+    }
+  },
+  computed: {
+    showCvvError (): boolean {
+      return !this.fieldsValidationState[Fields.CVV];
+    },
+    showExpirationDateError (): boolean {
+      return !this.fieldsValidationState[Fields.EXPIRATION_DATE];
+    },
+    showNumberError (): boolean {
+      return !this.fieldsValidationState[Fields.NUMBER];
     }
   },
   async created (): Promise<void> {
@@ -92,6 +135,8 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
         }
       });
 
+      this.addHostedFieldsEventListeners();
+
       this.hostedFieldsCreated();
     } catch (error) {
       this.$emit('error', error);
@@ -99,6 +144,7 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
   },
   beforeDestroy (): void {
     const placeOrderButton = this.getPlaceOrderButton();
+    this.removeHostedFieldsEventListeners();
 
     if (!placeOrderButton || !this.fSubmitHandler) {
       return;
@@ -107,6 +153,43 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
     placeOrderButton.removeEventListener('click', this.fSubmitHandler);
   },
   methods: {
+    addHostedFieldsEventListeners (): void {
+      if (!this.hostedFieldsInstance) {
+        throw new Error('Hosted fields instance is undefined');
+      }
+
+      this.fOnHostedFieldsBlur = this.onHostedFieldsBlur.bind(this);
+      this.hostedFieldsInstance.on('blur', this.fOnHostedFieldsBlur);
+
+      this.fOnHostedFieldsFocus = (event) => {
+        this.fieldsValidationState[event.emittedBy] = true;
+      };
+      this.hostedFieldsInstance.on('focus', this.fOnHostedFieldsFocus);
+    },
+    removeHostedFieldsEventListeners (): void {
+      if (!this.hostedFieldsInstance) {
+        return;
+      }
+
+      if (this.fOnHostedFieldsFocus) {
+        this.hostedFieldsInstance.off('focus', this.fOnHostedFieldsFocus);
+      }
+
+      if (this.fOnHostedFieldsBlur) {
+        this.hostedFieldsInstance.off('blur', this.fOnHostedFieldsBlur);
+      }
+    },
+    onHostedFieldsBlur (event: HostedFieldsEvent): void {
+      const fieldState = event.fields[event.emittedBy];
+
+      if ((!fieldState.isValid && !fieldState.isEmpty) || fieldState.isEmpty) {
+        this.fieldsValidationState[event.emittedBy] = false;
+      }
+
+      if (!this.showCvvError && !this.showExpirationDateError && !this.showNumberError) {
+        this.errorMessage = '';
+      }
+    },
     getPlaceOrderButton (): HTMLElement | null {
       return document.querySelector('.place-order-btn');
     },
@@ -122,13 +205,52 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
     },
     async submitHandler (event: MouseEvent): Promise<void> {
       event.preventDefault();
+      this.errorMessage = '';
 
       if (!this.hostedFieldsInstance || !this.isSelected) {
         return;
       }
 
-      const payload = await this.hostedFieldsInstance.tokenize();
-      this.$emit('success', payload);
+      try {
+        const payload = await this.hostedFieldsInstance.tokenize();
+        this.$emit('success', payload);
+      } catch (error) {
+        const braintreeError = error as BraintreeError;
+
+        if (braintreeError.details && braintreeError.details.invalidFieldKeys) {
+          braintreeError.details.invalidFieldKeys.forEach((key: string) => {
+            this.fieldsValidationState[key] = false;
+          })
+        }
+
+        switch (braintreeError.code) {
+          case 'HOSTED_FIELDS_FIELDS_EMPTY':
+            [Fields.EXPIRATION_DATE, Fields.NUMBER, Fields.CVV].forEach((key) => {
+              this.fieldsValidationState[key] = false;
+            })
+            this.errorMessage = this.$t('Missed required fields');
+            break;
+          case 'HOSTED_FIELDS_FIELDS_INVALID':
+            this.errorMessage = this.$t('Some fields are invalid');
+            break;
+          case 'HOSTED_FIELDS_TOKENIZATION_FAIL_ON_DUPLICATE':
+            // https://developer.paypal.com/braintree/docs/reference/request/client-token/generate#options.fail_on_duplicate_payment_method
+            this.errorMessage = this.$t('This payment method already exists in Your vault.');
+            break;
+          case 'HOSTED_FIELDS_TOKENIZATION_CVV_VERIFICATION_FAILED':
+            this.errorMessage = this.$t('CVV did not pass verification');
+            this.fieldsValidationState[Fields.CVV] = false;
+            break;
+          case 'HOSTED_FIELDS_FAILED_TOKENIZATION':
+            this.errorMessage = this.$t('Please, check credentials');
+            break;
+          case 'HOSTED_FIELDS_TOKENIZATION_NETWORK_ERROR':
+            this.errorMessage = this.$t('Network error. Please try again later');
+            break;
+          default:
+            this.errorMessage = this.$t('Something went wrong');
+        }
+      }
     }
   }
 })
@@ -139,11 +261,24 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
 
 .checkout-card {
   ._content {
-    margin-top: var(--spacer-sm);
+    margin: var(--spacer-sm) 0 var(--spacer-sm);
+    padding: 0 var(--spacer-sm);
   }
 
   ._row {
     display: flex;
+
+    &:first-child {
+      margin-bottom: var(--spacer-base);
+    }
+  }
+
+  ._input {
+    height: 32px;
+    display: block;
+    border-bottom: 1px solid var(--c-light);
+    width: 100%;
+    position: relative;
   }
 
   ._field {
@@ -153,34 +288,27 @@ export default (Vue as VueConstructor<Vue & InjectedServices>).extend({
     &:last-child {
       margin-right: 0;
     }
+
+    &.-error {
+      ._label {
+        color: var(--c-danger-variant);
+      }
+
+      ._input {
+        border-bottom-color: var(--c-danger);
+      }
+    }
   }
 
-  ._input {
-    height: 32px;
-    margin-bottom: 1em;
-    display: block;
-    background-color: transparent;
-    color: rgba(0, 0, 0, .87);
-    border: none;
-    border-bottom: 1px solid rgba(0, 0, 0, .26);
-    outline: 0;
-    width: 100%;
-    font-size: 16px;
-    padding: 0;
-    box-shadow: none;
-    border-radius: 0;
-    position: relative;
+  ._error-message {
+    font-size: 0.8em;
+    margin-top: 0.5em;
+    color: var(--c-danger-variant);
   }
 
   ::v-deep {
     .braintree-hosted-fields-focused {
-      border-bottom: 1px solid var(--c-light);
-      transition: all 200ms ease;
-    }
-
-    .braintree-hosted-fields-invalid {
-      border-bottom: 1px solid var(--c-danger);
-      transition: all 200ms ease;
+      border-bottom: 1px solid var(--c-primary);
     }
   }
 }
