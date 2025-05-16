b692559aea08aa8d22f17f1a1abc02c52c6df197
Anton Sylka <antonsylka1004@gmail.com>
2025-05-16 07:49:13 +0000

    Refs #34530: Google Pay - add support for payment method

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import { module } from './store'
 import { SET_PAYMENT_METHOD_NONCE, SN_BRAINTREE } from './store/mutation-types';
 import getComponentByMethodCode from './helpers/get-component-by-method-code.function';
 import supportedMethodsCodes from './types/SupportedMethodsCodes';
+import googlePayIcon from './assets/google-pay-mark.svg';
 import paypalIcon from './assets/paypal-icon.svg';
 import applePayIcon from './assets/apple-pay-icon.svg';
 import cardsIcon from './assets/cards-icon.png';
@@ -52,6 +53,10 @@ export const Braintree: StorefrontModule = function ({ app, store }) {
           case supportedMethodsCodes.CARD:
           case supportedMethodsCodes.MAGENTO1_CARD:
             method.icon = cardsIcon;
+            break;
+          case supportedMethodsCodes.GOOGLE_PAY:
+            method.icon = googlePayIcon;
+            break;
         }
       })
     };
